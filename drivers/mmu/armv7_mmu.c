363a29b5d92e740f64ed848e0a15eeebe3ec6bbc
Christopher Karle <ckarle0@gmail.com>
2015-09-07 19:06:06 -0500

    remove unneeded include

--- drivers/mmu/armv7_mmu.c
+++ drivers/mmu/armv7_mmu.c
@@ -25,7 +25,6 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ****************************************************************************/
-#include <string.h>
 #include "armv7_mmu.h"
 
 /****************************************************************************
